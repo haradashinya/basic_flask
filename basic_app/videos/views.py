75cadac332ff08ae7a5d819defc9f75438f095e0
okamurayasuyuki <haradashinya@gmail.com>
2013-11-01 06:04:49 +0000

    fix

--- basic_app/videos/views.py
+++ basic_app/videos/views.py
@@ -1,6 +1,7 @@
 #coding: utf-8
 from basic_app.videos import bp
-# from basic_app.users.models import User
+from basic_app.users.models import User
+from basic_app.videos.models import Video
 from basic_app import db
 from flask import render_template,redirect,request,session
 
@@ -12,11 +13,15 @@ from flask import render_template,redirect,request,session
 # def hello():
 # 	return "llflfll"
 
-@bp.route("/new_video")
+@bp.route("/new_video",methods=["GET","POST"])
 def new_video_view():
 	return render_template("new_video.html")
 
 
-@bp.route("/")
+@bp.route("",methods=["GET","POST"])
 def hello():
-	return "a"
+	if request.method == "GET":
+		return "show all videos"
+	elif request.method == "POST":
+		return "created"
+
